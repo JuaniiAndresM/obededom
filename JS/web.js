241ffa81c35ce63123ab4613989636efbcaf9f52
Juan Andrés Morena <64050263+JuaniiAndresM@users.noreply.github.com>
2021-09-23 16:47:23 -0300

    Plugins + Slider Index + Responsive

---
+++ JS/web.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    $('#header').load('/web/header.html');
+    $('#footer').load('/web/footer.html');
+
+    createSlider();
+});
+
+function createSlider(){
+    new Glider(document.querySelector('.slider-content'), {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        scrollLock: false,
+        dots: '.slider-indicators',
+        arrows: {
+            prev: '.slider-anterior',
+            next: '.slider-siguiente'
+        },
+        responsive: [
+            {
+              // screens greater than >= 775px
+              breakpoint: 200,
+              settings: {
+
+                slidesToShow: '1',
+                slidesToScroll: '1',
+                itemWidth: 150,
+                duration: 0.25
+              }
+            },{
+                // screens greater than >= 1024px
+                breakpoint: 700,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  itemWidth: 150,
+                  duration: 0.25
+                }
+              },{
+              // screens greater than >= 1024px
+              breakpoint: 1050,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                itemWidth: 150,
+                duration: 0.25
+              }
+            },{
+                // screens greater than >= 1024px
+                breakpoint: 1400,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 4,
+                  itemWidth: 150,
+                  duration: 0.25
+                }
+              }
+        ]
+    });
+}
