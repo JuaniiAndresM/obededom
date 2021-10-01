48c80a20c1dd113a7b7c9231824c8c8ef34f7e4b
Juan Andrés Morena <64050263+JuaniiAndresM@users.noreply.github.com>
2021-10-01 01:18:52 -0300

    Server

--- Database/server.php
+++ Database/server.php
@@ -3,6 +3,7 @@
 
 //Abrir nueva conexion con server MySQL
 $mysqli = new mysqli('localhost','root','root','obededom');
+//$mysqli = new mysqli('obededompropiedades.com.uy','obededom_giovanna','giovanna2021*','obededom_obededom');
 
 //Error de conexion
 if ($mysqli->connect_error) {
