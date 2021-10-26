429dca04b7b42fb29cfad3aeb8757b5b25afbca5
Juan Andrés Morena <64050263+JuaniiAndresM@users.noreply.github.com>
2021-10-26 15:59:54 -0300

    Problemas tildes

--- Database/server.php
+++ Database/server.php
@@ -5,6 +5,8 @@
 //$mysqli = new mysqli('localhost','root','root','obededom');
 $mysqli = new mysqli('obededompropiedades.com.uy','obededom_giovanna','giovanna2021','obededom_obededom', 33061);
 
+$mysqli->set_charset("utf8");
+
 //Error de conexion
 if ($mysqli->connect_error) {
     die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
